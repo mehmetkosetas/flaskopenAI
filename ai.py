96f36aadaff85feef18c895d09afe4dd590a8a7d
Mehmet Köşetaş <90596132+mehmetkosetas@users.noreply.github.com>
2025-04-15 15:58:51 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,7 +7,7 @@ from sklearn.cluster import KMeans
 app = Flask(__name__)
 
 # Set OpenAI API Key
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("openAIKEY")
 
 # 1. CSV Dosyasını Oku ve Veriyi İşle
 df = pd.read_csv("study_sessions_detailed.csv")
